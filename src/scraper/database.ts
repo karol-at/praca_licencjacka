738ad01eaf393cfb2f75d57c0c7b7fc488ec3d87
Karol Atys <atys@atys.nazwa.pl>
2025-03-05 23:33:23 +0100

    add db creation and destruction functions

--- src/scraper/database.ts
+++ src/scraper/database.ts
@@ -1,3 +1,44 @@
-import { DatabaseSync } from 'node:sqlite'
+import { DatabaseSync } from "node:sqlite";
+import { WarsawDataPoint } from "./warsaw/WarsawConverter.ts";
 
-const db = new DatabaseSync('base.db')
+const db = new DatabaseSync("base.db");
+
+const createTables = (): void =>
+  db.exec(
+    `
+      CREATE TABLE warsawData (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        latitude REAL,
+        longitude REAL,
+        time TEXT,
+        brigade INTEGER,
+        vehicleNumber INTEGER
+      );
+      CREATE TABLE gdanskData (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        latitude REAL,
+        longitude REAL,
+        generated TEXT,
+        routeShortName TEXT,
+        tripId INTEGER,
+        routeId INTEGER,
+        headsign TEXT,
+        vehicleCode TEXT,
+        vehicleService TEXT,
+        vehicleId TEXT,
+        speed REAL,
+        direction REAL,
+        delay INTEGER,
+        scheduledTripStartTime TEXT,
+        gpsQuality INTEGER
+      )
+    `,
+  );
+
+const dropTables = (): void =>
+  db.exec(
+    `
+      DROP TABLE warsawData;
+      DROP TABLE gdanskData
+    `,
+  );
