4d65ea19bd8a704c8e740115f6454873520ae09b
karol-at <karol.atys@gmail.com>
2025-05-21 19:42:02 +0200

    Add error handling for lacking env variables

--- src/analyst/env.py
+++ src/analyst/env.py
@@ -2,6 +2,14 @@ import dotenv
 
 # dotenv files parsed into variables to avoid repeat parsing
 
-path = dotenv.dotenv_values()['DIRECTORY'].replace('/', '\\')
-gtfs = dotenv.dotenv_values()['GTFS'].replace('/','\\')
+path = dotenv.dotenv_values()['DIRECTORY']
+if path == None:
+    raise ValueError("DIRECTORY not defined in .env")
+path = path.replace('/', '\\')
+
+gtfs = dotenv.dotenv_values()['GTFS']
+if gtfs == None:
+    raise ValueError("GTFS not defined in .env")
+gtfs = gtfs.replace('/', '\\')
+
 api_key = dotenv.dotenv_values()['APIKEY']
