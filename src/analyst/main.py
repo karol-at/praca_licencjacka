73585e892552dd84eb49e67bb12d8c99a86c70e6
karol-at <karol.atys@gmail.com>
2025-07-24 10:14:48 +0200

    add line joininig loop

--- src/analyst/main.py
+++ src/analyst/main.py
@@ -1,17 +1,30 @@
 import arcpy
 import stop_preparator as sp
+import connector as con
 import env
 import os
 import config
-from typing import Literal
 
 dates = os.listdir(env.path)
 for date in dates:
     for city in config.cities:
-        df = sp.get_stops(f'{env.path}/{date}/gtfs/{city}.zip', city)
+
+        if city not in ['warsaw', 'gdansk']:
+            raise ValueError()
+        
+        cwd = f'{env.path}\\{date}'
+
+        df = sp.get_stops(f'{env.path}\\{date}\\gtfs\\{city}.zip', city)
         lines = sp.split_lines(df)
-        gdb = arcpy.management.CreateFileGDB(f'{env.path}\\{date}', 'base.gdb')
-        processed_lines = {}
+
+        gdb = arcpy.management.CreateFileGDB(cwd, 'base.gdb')
+
+        arcpy.env.workspace = f'{cwd}\\base.gdb'
+
+        processed_lines: list[str] = []
         for line in lines:
-            processed_lines[line] = sp.stops_to_features(gdb, city, lines[line])
+            processed_lines += sp.stops_to_features(cwd, city, lines[line])
+            
+        for line in processed_lines:
+            con.join_line(cwd, line)
             
