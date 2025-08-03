d56237b645b262086287acdc2cf6d8db0ef1f8f7
karol-at <karol.atys@gmail.com>
2025-08-03 18:59:03 +0200

    remove alternative route names due to changes in name selection structure and fixes to GeoJSON conversion

--- src/analyst/config.py
+++ src/analyst/config.py
@@ -8,8 +8,3 @@ lines = {
 route_aliases = {
     'Muzeum II Wojny Światowej': 'Muzeum II Wojny'
 }
-
-alternative_route_names = {
-    '731 -> Starostwo Powiatowe': '731 -> Os. Młodych',
-    '158 -> CH  Reduta': '158 -> CH Reduta'
-}
